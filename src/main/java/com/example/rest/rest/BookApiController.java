a214519d5b42fbc8ebc64653dd88cf15b1d786cb
TarasH1 <research010@gmail.com>
2021-04-22 19:20:38 +0300

    basic crud operations

--- src/main/java/com/example/rest/rest/BookApiController.java
+++ src/main/java/com/example/rest/rest/BookApiController.java
@@ -4,11 +4,11 @@ import com.example.rest.entity.Author;
 import com.example.rest.entity.Book;
 import com.example.rest.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @RestController
 @RequestMapping(path = "/api/book")
@@ -20,5 +20,27 @@ public class BookApiController {
     @PostMapping
     public Book saveBook(@Validated @RequestBody Book book) {
         return bookRepository.save(book);
+    }
+
+    @PutMapping(value="/{id}")
+    public Book update(@PathVariable Long id, @RequestParam String name, Author author, BigDecimal publishedAmount,
+                         BigDecimal soldAmount) throws Exception {
+
+        Book book = bookRepository.findById(id).orElseThrow(()->new Exception("No book with ID : " + id));
+
+        book.setBookName(name);
+        book.setAuthor(author);
+        book.setPublishedAmount(publishedAmount);
+        book.setSoldAmount(soldAmount);
+
+        return book;
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        bookRepository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
